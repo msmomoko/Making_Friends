258d5563cf314af45b0cc75f0e7e29b400fd0401
msmomoko <b1920590@planet.kanazawa-it.ac.jp>
2021-11-27 16:13:05 +0900

    [add]登録時のメール認証

--- routes/web.php
+++ routes/web.php
@@ -4,19 +4,21 @@
 
 
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
-Route::get('/', 'RecruitmentController@index');
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/recruitments', 'RecruitmentController@index');
-Route::get('/recruitments/create', 'RecruitmentController@create');
-Route::get('/recruitments/{recruitment}', 'RecruitmentController@show');
-Route::post('/recruitments', 'RecruitmentController@store');
-Route::get('/recruitments/{recruitment}/edit', 'RecruitmentController@edit');
-Route::put('/recruitments/{recruitment}', 'RecruitmentController@update');
-Route::delete('/recruitments/{recruitment}', 'RecruitmentController@delete');
-
-Route::post('/recruitments/{recruitment}/favorites', 'FavoriteController@store')->name('favorites');
-Route::post('/recruitments/{recruitment}/unfavorites', 'FavoriteController@destroy')->name('unfavorites');
-
-Route::post('/recruitments/{recruitment}/comment', 'CommentController@store');
+Route::middleware('verified')->group(function() {
+    Route::get('/', 'RecruitmentController@index');
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::get('/recruitments', 'RecruitmentController@index');
+    Route::get('/recruitments/create', 'RecruitmentController@create');
+    Route::get('/recruitments/{recruitment}', 'RecruitmentController@show');
+    Route::post('/recruitments', 'RecruitmentController@store');
+    Route::get('/recruitments/{recruitment}/edit', 'RecruitmentController@edit');
+    Route::put('/recruitments/{recruitment}', 'RecruitmentController@update');
+    Route::delete('/recruitments/{recruitment}', 'RecruitmentController@delete');
+    
+    Route::post('/recruitments/{recruitment}/favorites', 'FavoriteController@store')->name('favorites');
+    Route::post('/recruitments/{recruitment}/unfavorites', 'FavoriteController@destroy')->name('unfavorites');
+    
+    Route::post('/recruitments/{recruitment}/comment', 'CommentController@store')->middleware('verified');
+});
